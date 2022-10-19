bf92433eefe7d62b205233f06fc6dec0b877e54d
Franciszek Malinka <franciszek.malinka@gmail.com>
2022-10-19 16:50:32 +0200

    Fix after previous commit + clang-format

--- komfydb/storage/db_file_iterator.h
+++ komfydb/storage/db_file_iterator.h
@@ -1,27 +1,32 @@
 #ifndef __DB_FILE_ITERATOR_H__
 #define __DB_FILE_ITERATOR_H__
 
-
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 
-#include "komfydb/storage/tuple.h"
+#include "komfydb/common/tuple.h"
+
+
+namespace {
+
+using komfydb::common::Tuple;
+
+};  // namespace
 
 
 namespace komfydb::storage {
 
 class DbFileIterator {
- public: 
+ public:
   virtual absl::Status Open();
 
   virtual void Close();
 
   virtual absl::StatusOr<bool> HasNext();
-  
+
   virtual absl::StatusOr<Tuple> Next();
 };
 
-};
-
+};  // namespace komfydb::storage
 
 #endif  // __DB_FILE_ITERATOR_H__
