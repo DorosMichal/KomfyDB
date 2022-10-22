6aaaa5b2612704bd37e57d0c00686e839a9d8a35
Franciszek Malinka <franciszek.malinka@gmail.com>
2022-10-22 23:14:39 +0200

    Implemented tuple.cc, made Field an abstract class

--- komfydb/common/tuple.h
+++ komfydb/common/tuple.h
@@ -17,21 +17,28 @@ class Tuple {
  private:
   TupleDesc td;
 
+  std::vector<Field> fields;
+
  public:
   Tuple(const TupleDesc& td);
 
   TupleDesc GetTupleDesc();
 
-  absl::StatusOr<Field> GetField(int i);
+  // TODO it would be much nicer to have absl::StatusOr<Field> instead, but
+  // unfortunately it cannot be done as StatusOr tries to instatiate
+  // the object of the Field type (which is an abstract class).
+  // Try changing Field class so it can work (and don't break the abstraction).
+  absl::StatusOr<Field*> GetField(int i);
 
   absl::Status SetField(int i, Field f);
 
   operator std::string() const;
 
-  std::vector<Field> GetFields();
+  // TODO(Iterator)
+  // std::vector<Field> GetFields();
 
   // Wtf should this do? lol
-  void ResetTupleDesc(TupleDesc td);
+  // void ResetTupleDesc(TupleDesc td);
 };
 
 };  // namespace komfydb::common
