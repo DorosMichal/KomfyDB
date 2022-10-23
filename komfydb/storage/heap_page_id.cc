0e94f37fbc7836ec9d53fcc0f4c0ee67e6535c2d
Franciszek Malinka <franciszek.malinka@gmail.com>
2022-10-23 19:21:48 +0200

    Clang formatted

--- komfydb/storage/heap_page_id.cc
+++ komfydb/storage/heap_page_id.cc
@@ -7,15 +7,15 @@
 namespace komfydb::storage {
 
 HeapPageId::HeapPageId(int table_id, int page_no)
-  : table_id(table_id), page_no(page_no) {}
+    : table_id(table_id), page_no(page_no) {}
 
 std::vector<uint8_t> HeapPageId::Serialize() {
   std::vector<uint8_t> data(sizeof(table_id) + sizeof(page_no));
-  ((int *)&data)[0] = table_id;
-  ((int *)&data)[1] = page_no;
+  ((int*)&data)[0] = table_id;
+  ((int*)&data)[1] = page_no;
   return data;
 }
-  
+
 int HeapPageId::GetTableId() const {
   return table_id;
 }
@@ -28,4 +28,4 @@ bool HeapPageId::operator==(const PageId& p) const {
   return table_id == p.GetTableId() && page_no == p.GetPageNumber();
 }
 
-};
+};  // namespace komfydb::storage
