771b9e4c8f791cfbb86c00008c88edc8205b1c62
Franciszek Malinka <franciszek.malinka@gmail.com>
2022-10-23 14:35:37 +0200

    Added record

---
+++ komfydb/common/tuple_test.cc
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+
+#include "komfydb/common/tuple.h"
+#include "komfydb/common/tuple_desc.h"
+#include "komfydb/common/int_field.h"
+#include "komfydb/common/string_field.h"
+
+namespace komfydb::common {
+
+TEST(Tuple, StringConversion) {
+  Type int_t(Type::INT);
+  Type str_t(Type::STRING);
+  std::vector<Type> tv{int_t, str_t};
+  std::vector<std::string> nv{"f1", "f2"};
+
+  TupleDesc td(tv, nv);
+  Tuple tuple(td);   
+
+  EXPECT_OK(tuple.SetField(0, Field()));
+  
+}
+
+};
