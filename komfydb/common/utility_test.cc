0825d9391f6e1f6614017bf29d36e8100939c29a
Franciszek Malinka <franciszek.malinka@gmail.com>
2022-11-02 00:30:50 +0100

    Added test for status macros (so we know it works)

--- komfydb/common/utility_test.cc
+++ komfydb/common/utility_test.cc
@@ -1,6 +1,7 @@
 #include "gmock/gmock-matchers.h"
 #include "gtest/gtest.h"
 
+#include "komfydb/common/status_macros.h"
 #include "komfydb/common/utility.h"
 
 namespace {
@@ -12,6 +13,21 @@ TEST(Utility, JoinVectors) {
   std::vector<int> v1{1, 2, 3}, v2{4, 5, 6, 7}, v3;
   JoinVectors(v1, v2, v3);
   EXPECT_THAT(v3, ElementsAre(1, 2, 3, 4, 5, 6, 7));
+}
+
+absl::StatusOr<int> MaybeFunction() {
+  return 42;
+}
+
+absl::StatusOr<int> SampleFunction() {
+  ASSIGN_OR_RETURN(int x, MaybeFunction());
+  return x;
+}
+
+TEST(StatusMacros, Check) {
+  auto x = SampleFunction();
+  EXPECT_TRUE(x.ok());
+  EXPECT_EQ(*x, 42);
 }
 
 };  // namespace
