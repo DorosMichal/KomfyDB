851737613341c6087301e4af8a7c3af17e2d13c0
kacper776 <kacper1342@gmail.com>
2023-01-02 18:15:56 +0100

    Filter and Predicate implementation

---
+++ komfydb/execution/filter.cc
@@ -0,0 +1,62 @@
+#include "komfydb/execution/filter.h"
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+
+#include "komfydb/execution/op.h"
+#include "komfydb/utils/status_macros.h"
+
+namespace {
+
+using namespace komfydb::common;
+
+}  // namespace
+
+namespace komfydb::execution {
+
+Filter::Filter(std::unique_ptr<OpIterator> child, Predicate predicate,
+               TupleDesc& td)
+    : OpIterator(td),
+      child(std::move(child)),
+      predicate(std::move(predicate)) {}
+
+absl::StatusOr<std::unique_ptr<Filter>> Filter::Create(
+    std::unique_ptr<OpIterator> child, Predicate predicate) {
+  TupleDesc* td = child->GetTupleDesc();
+  /* Check if td has field specified in predicate */
+  RETURN_IF_ERROR(td->GetFieldType(predicate.GetLField()).status());
+  if (predicate.GetType() == Predicate::Type::COL_COL) {
+    RETURN_IF_ERROR(td->GetFieldType(predicate.GetRField()).status());
+  }
+
+  return std::unique_ptr<Filter>(
+      new Filter(std::move(child), std::move(predicate), *td));
+}
+
+Predicate* Filter::GetPredicate() {
+  return &predicate;
+}
+
+absl::Status Filter::Open() {
+  return child->Open();
+}
+
+void Filter::Close() {
+  child->Close();
+}
+
+absl::Status Filter::Rewind() {
+  return child->Rewind();
+}
+
+absl::Status Filter::FetchNext() {
+  ITERATE_RECORDS(child, record) {
+    if (predicate.Evaluate(*record.value())) {
+      next_record = std::move(record.value());
+      return absl::OkStatus();
+    }
+  }
+  return record.status();
+}
+
+}  // namespace komfydb::execution
---
+++ komfydb/execution/filter.h
@@ -0,0 +1,34 @@
+#ifndef __FILTER_H__
+#define __FILTER_H__
+
+#include "komfydb/common/tuple_desc.h"
+#include "komfydb/execution/op_iterator.h"
+#include "komfydb/execution/predicate.h"
+
+namespace komfydb::execution {
+
+class Filter : public OpIterator {
+ public:
+  static absl::StatusOr<std::unique_ptr<Filter>> Create(
+      std::unique_ptr<OpIterator> child, Predicate predicate);
+
+  Predicate* GetPredicate();
+
+  absl::Status Open() override;
+
+  void Close() override;
+
+  absl::Status Rewind() override;
+
+ private:
+  std::unique_ptr<OpIterator> child;
+  Predicate predicate;
+
+  Filter(std::unique_ptr<OpIterator> child, Predicate predicate, TupleDesc& td);
+
+  absl::Status FetchNext() override;
+};
+
+}  // namespace komfydb::execution
+
+#endif
