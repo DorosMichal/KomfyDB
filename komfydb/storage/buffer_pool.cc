7c3ff1cc15701e5a8faf3a59a6fb9a0da09b3019
Franciszek Malinka <franciszek.malinka@gmail.com>
2023-01-08 14:57:07 +0100

    Removed trash from buffer_pool.cc

--- komfydb/storage/buffer_pool.cc
+++ komfydb/storage/buffer_pool.cc
@@ -9,11 +9,7 @@ namespace komfydb::storage {
 
 absl::StatusOr<Page*> BufferPool::GetPage(TransactionId tid, PageId pid,
                                           Permissions perm) {
-  static int cnt = 0;
   if (!page_pool.count(pid)) {
-    LOG(INFO) << "Reading for the " << ++cnt << " time.";
-    LOG(INFO) << page_pool.contains(pid) << " " << pid.GetTableId() << " "
-              << pid.GetPageNumber();
     if (page_pool.size() == pages_cnt) {
       // return absl::InvalidArgumentError("Cannot read another file to the pool");
       LOG(ERROR) << "Buffer run out of space\n";
@@ -21,9 +17,7 @@ absl::StatusOr<Page*> BufferPool::GetPage(TransactionId tid, PageId pid,
     }
     LOG(INFO) << "Getting page from disk: " << static_cast<std::string>(pid);
     ASSIGN_OR_RETURN(DbFile * file, catalog->GetDatabaseFile(pid.GetTableId()));
-    LOG(INFO) << "Siema xd " << page_pool.contains(pid);
     ASSIGN_OR_RETURN(page_pool[pid], file->ReadPage(pid));
-    LOG(INFO) << "Siema xd " << page_pool.contains(pid);
   }
 
   return page_pool.at(pid).get();
